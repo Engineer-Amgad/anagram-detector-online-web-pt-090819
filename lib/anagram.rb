31eba2642650e1ef0e05381a8afde27223389601
Amgad Malik <amgadmalik@yahoo.com>
2019-10-10 00:28:40 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :anagram_word 
+  
+  def initialize(word)
+    @anagram_word = word
+  end 
+  
+  def match(array)
+    anagram_word_array = anagram_word.split
+    array.split(" ")
+    
+  end 
+end 
